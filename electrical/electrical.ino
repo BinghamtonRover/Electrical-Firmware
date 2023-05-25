57c0aaf2d0e93a94661a262752e9ca55582d5153
Levi Lesches <levilesches@gmail.com>
2023-05-25 03:54:54 -0400

    Slight cleanup and more logging

--- electrical/electrical.ino
+++ electrical/electrical.ino
@@ -6,20 +6,27 @@
 #define ELECTRICAL_COMMAND_ID 0x63
 #define ELECTRICAL_DATA_ID    0x13
 
+#define CAN_SEND_INTERVAL 500
+
 void handleCommand(const uint8_t* data, int length);
 BurtCan can(ELECTRICAL_COMMAND_ID, handleCommand);
 BurtSerial serial(handleCommand, Device::Device_ELECTRICAL);
+unsigned long nextSendTime;
 
 void setup() { 
 	Serial.begin(9600);
-	Serial.println("Initializing...");
+	Serial.println("Initializing Electrical subsystem");
 
+	Serial.println("Initializing communications...");
 	can.setup();
+	nextSendTime = millis() + CAN_SEND_INTERVAL;
 
+	Serial.println("Initializing LED buttons...");
 	red.setup();
 	yellow.setup();
 	green.setup();
 
+	Serial.println("Initializing sensors...");
 	currentSensor.setup();
 
 	Serial.println("Electrical firmware initialized.");
@@ -29,22 +36,29 @@ void loop() {
 	can.update();
 	serial.update();
 
+	Serial.print("Current value: ");
+	Serial.print(currentSensor.getCurrent());
+	Serial.println(" mA");
+
+	/* DISABLED: The LED buttons are not implemented for the '23 competition
 	red.update();
 	yellow.update();
 	green.update();
+	*/
 
 	sendData();
 }
 
 void sendData() {
 	float current = currentSensor.getCurrent();
-	ElectricalData data;
+	ElectricalData data = ElectricalData_init_zero;
 	data.battery_current = current;
 	can.send(ELECTRICAL_DATA_ID, &data, ElectricalData_fields);
 }
 
 void handleCommand(const uint8_t* data, int length) {
 	auto command = BurtProto::decode<ElectricalCommand>(data, length, ElectricalCommand_fields);
+  Serial.println(command.status);
 	switch(command.status) {
 		case RoverStatus_DISCONNECTED: break;
 		case RoverStatus_IDLE: 
@@ -65,4 +79,3 @@ void handleCommand(const uint8_t* data, int length) {
 			break;
 	}
 }
-
